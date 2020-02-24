4c59e22f06d2ea34d2632948deaed52c2c550179
Shuyi Qi <qishuyi0525@gmail.com>
2020-02-24 14:11:41 -0600

    Separate column addition code into helper file

---
+++ columns_addition.R
@@ -0,0 +1,91 @@
+##Activeness Column
+dogs_data <- dogs_data %>% unite("Activeness", c(IC1_Name, IC2_Name), remove = T)
+dogs_data <- dogs_data %>% mutate(Activeness = gsub("NA_", "", Activeness))
+dogs_data <- dogs_data %>% mutate(Activeness = gsub("_NA", "", Activeness))       
+
+##Activity Column and Changing Long Column Names
+dogs_data <- dogs_data %>% unite("Activity", c(IC1_Value, IC2_Value), remove = T)
+dogs_data <- dogs_data %>% mutate(Activity = gsub("NA_", "", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("_NA", "", Activity))                                  
+
+##Changing Name of Behavior in Activity Column
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Dog interaction", "Dog Int", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Interacting with object", "Object Int", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Walking Around", "Walking", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Out of view", "Out of View", Activity))
+
+##Day_of_Week Column
+dogs_data <- mutate(dogs_data, Day_of_Week = wday(Date, label = TRUE))
+
+#Adding Food Column
+dogs_data <- cbind(dogs_data, Food = dogs_data$Day_of_Week) 
+dogs_data$Food <- as.character(dogs_data$Food)
+
+for(day in c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")){
+  
+  if(day == "Fri"){
+    dogs_data$Food[dogs_data$Food == day] <- "Guinea Pig"
+  }
+  
+  else if(day == "Wed" | day == "Sun"){
+    dogs_data$Food[dogs_data$Food == day] <- "Bones"
+  }
+  
+  else{
+    dogs_data$Food[dogs_data$Food == day] <- "Ground Meat"
+  }
+}
+
+##Season Column
+dogs_data <- cbind(dogs_data, Season = dogs_data$Month)
+dogs_data$Month <- as.character(dogs_data$Month)
+
+for(month in c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")){
+  
+  if(month == "1" | month == "2" | month == "12"){
+    dogs_data$Season[dogs_data$Season == month] <- "Winter"
+  }
+  else if(month == "3" | month == "4" | month == "5"){
+    dogs_data$Season[dogs_data$Season == month] <- "Spring"
+  }
+  else if(month == "6" | month == "7" | month == "8"){
+    dogs_data$Season[dogs_data$Season == month] <- "Summer"
+  }
+  
+  else{
+    dogs_data$Season[dogs_data$Season == month] <- "Fall"
+  }}
+
+##Temperature/Weather Columns
+#Cleaning weather_temp dataset
+weather_temp <- read_csv("Weather_Temperature.csv")
+weather_temp$DATE <- as.Date(weather_temp$DATE, format = "%m/%d/%y")
+weather_temp <- weather_temp %>% select(-c(STATION, NAME, TOBS))
+weather_temp <- weather_temp %>%
+  mutate(TAVG_C = round((TAVG-32)*5/9)) %>%
+  mutate(TMAX_C = round((TMAX-32)*5/9)) %>%
+  mutate(TMIN_C = round((TMIN-32)*5/9))
+weather_temp <- weather_temp %>% slice(1:717)
+
+#Merge/Further Cleaning
+dogs_data <- left_join(dogs_data, weather_temp, by = c("Date" = "DATE"))
+dogs_data <- dogs_data %>% select(-c(WT02, WT06, WT07, WT09, WT11))
+
+##Average Temperature Level Column
+dogs_data <- within(dogs_data, Temp_Level <- as.integer(cut(TAVG, quantile(TAVG, probs=0:10/10), include.lowest=TRUE)))
+dogs_data$Temp_Level <- as.character(dogs_data$Temp_Level)
+dogs_data$Temp_Level <- factor(dogs_data$Temp_Level, c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+
+##Average of Average-max Temperature column and its temperature level
+dogs_data <- mutate(dogs_data, TAVG_TMAX_avg = (dogs_data$TMAX + dogs_data$TAVG)/2)
+dogs_data <- within(dogs_data, Temp_Level2 <- as.integer(cut(TAVG_TMAX_avg, quantile(TAVG_TMAX_avg, probs=0:10/10), include.lowest=TRUE)))
+dogs_data$Temp_Level2 <- as.character(dogs_data$Temp_Level2)
+dogs_data$Temp_Level2 <- factor(dogs_data$Temp_Level2, c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10"))
+
+##Add column for six month intervals
+get_interval <- function(inputDate) {
+  six_month_intervals[(as.yearmon(inputDate)-
+                         as.yearmon(start_date))*12/6 + 1]
+}
+get_interval <- Vectorize(get_interval)
+dogs_data <- mutate(dogs_data, Six_Month_Interval = get_interval(Date))
