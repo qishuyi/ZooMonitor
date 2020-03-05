e8879793ce265d5ef3d655c90a1c4894e0dc656a
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-03-05 15:50:28 -0600

    Fixed dogs cleaning procedure to match cattle procedure

--- dogs_cleaning.R
+++ dogs_cleaning.R
@@ -8,11 +8,11 @@ library(stringr)
 dogs_data <- read_csv("https://raw.githubusercontent.com/qishuyi/ZooMonitor/master/report_study_1579790635.csv", col_types = cols(Notes = col_character()))
 
 
-#Data Cleaning
+#Removing Unnecessary Columns
 dogs_data <- dogs_data %>% select(-`Configuration Name`, -Observer,-DeviceID,
                                   -DateTime, -`Grid Size`, -`Image Size`,-`Channel Type`,
                                   -`Project Animals`, -Duration) 
-
+#Renaming Columns
 dogs_data <- dogs_data %>% rename(Session_Start_Time = `Session Start Time`,
                                   Session_End_Time = `Session End Time`, 
                                   Name = `Focal Name`,
@@ -23,17 +23,45 @@ dogs_data <- dogs_data %>% rename(Session_Start_Time = `Session Start Time`,
                                   IC2_Name = `Interval Channel 2 Name`,
                                   IC2_Value = `Interval Channel 2 Value`)
 
-dogs_data <- dogs_data %>% filter(!(is.na(IC1_Value) & is.na(IC2_Value)))
+#Uniting all IC Names columns to a single column named Activeness  
+dogs_data <- dogs_data %>% unite("Activeness", c(IC1_Name, IC2_Name), remove = T)
 
-#Split each row with both active and inactive behaviors into two rows
-duplicate_indices <- which((!is.na(dogs_data$IC1_Value)) & (!is.na(dogs_data$IC2_Value)))
-for (row_num in duplicate_indices) {
-  duplicate_row <- dogs_data[row_num,]
-  dogs_data[row_num, 15] <- NA
-  dogs_data[row_num, 16] <- NA
-  duplicate_row[, 12] <- NA
-  duplicate_row[, 13] <- NA
-  dogs_data <- rbind(dogs_data, duplicate_row)
-}
+#Uniting all IC Values columns to a single column named Activity  
+dogs_data <- dogs_data %>% unite("Activity", c(IC1_Value, IC2_Value), remove = T)
+
+
+#Filtering out rows without behavioral observations
+dogs_data <-  dogs_data %>% filter(!(Activeness == "NA_NA")) 
+
+
+#Fixing NA Labels for Activeness Column
+dogs_data <- dogs_data %>% mutate(Activeness = gsub("NA_", "", Activeness))
+dogs_data <- dogs_data %>% mutate(Activeness = gsub("_NA", "", Activeness))       
+
+
+#Fixing NA Labels for Activity Column
+dogs_data <- dogs_data %>% mutate(Activity = gsub("NA_", "", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("_NA", "", Activity))                                  
+
+
+#Splitting rows with multiple behavioral observations                                
+dogs_data <- separate_rows(dogs_data, Activeness, Activity, sep  = "_")
+
+
+#Relabeling Behaviors in Activity Column
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Dog interaction", "Dog Int", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Interacting with object", "Object Int", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Walking Around", "Walking", Activity))
+dogs_data <- dogs_data %>% mutate(Activity = gsub("Out of view", "Out of View", Activity))
+
+
+
+
+
+
+
+
+
+
 
 
