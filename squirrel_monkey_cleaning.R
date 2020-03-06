65d2f94c733e685e35b99a262f7df3d649786f24
Hinako Minagi <minagihi@grinnell.edu>
2020-03-06 14:15:22 -0600

    changing the squirrel moneky data

--- squirrel_monkey_cleaning.R
+++ squirrel_monkey_cleaning.R
@@ -3,4 +3,4 @@ library(tidyr)
 library(dplyr)
 library(stringr)
 
-squirrel_monkey_data <- read_csv("https://raw.githubusercontent.com/qishuyi/ZooMonitor/master/report_study_1583445158.csv", col_types = cols(Notes = col_character()))
+sq_monkey_data <- read_csv("https://raw.githubusercontent.com/qishuyi/ZooMonitor/master/report_study_1583445158.csv", col_types = cols(Notes = col_character()))
