21f5ba36d4f7575451453a3dd549605b8bb6dcec
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-03-06 15:51:30 -0600

    Updated dogs cleaning

--- dogs_cleaning.R
+++ dogs_cleaning.R
@@ -7,16 +7,12 @@ library(stringr)
 #Importing Dogs Data
 dogs_data <- read_csv("https://raw.githubusercontent.com/qishuyi/ZooMonitor/master/report_study_1579790635.csv", col_types = cols(Notes = col_character()))
 
-
-#Removing Unnecessary Columns
-dogs_data <- dogs_data %>% select(-`Configuration Name`, -Observer,-DeviceID,
-                                  -DateTime, -`Grid Size`, -`Image Size`,-`Channel Type`,
-                                  -`Project Animals`, -Duration) 
 #Renaming Columns
 dogs_data <- dogs_data %>% rename(Session_Start_Time = `Session Start Time`,
                                   Session_End_Time = `Session End Time`, 
                                   Name = `Focal Name`,
                                   Frame_Number = `Frame Number`,
+                                  Channel_Type = `Channel Type`,
                                   IC1_Name = `Interval Channel 1 Name`,
                                   IC1_Value = `Interval Channel 1 Value`,
                                   IC1_Social_Modifier = `Interval Channel 1 Social Modifier`,
@@ -55,7 +51,10 @@ dogs_data <- dogs_data %>% mutate(Activity = gsub("Walking Around", "Walking", A
 dogs_data <- dogs_data %>% mutate(Activity = gsub("Out of view", "Out of View", Activity))
 
 
-
+#Removing Unnecessary Columns
+dogs_data <- dogs_data %>% select(-`Configuration Name`, -Observer,-DeviceID,
+                                  -DateTime, -`Grid Size`, -`Image Size`,
+                                  -`Project Animals`, -Duration, -Channel_Type) 
 
 
 
