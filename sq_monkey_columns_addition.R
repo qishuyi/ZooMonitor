09ef46bffe046b87dc342667a53c7cd445b36df3
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-03-11 17:55:17 -0500

    Fixed Sq_Monkey misrecorded rows

--- sq_monkey_columns_addition.R
+++ sq_monkey_columns_addition.R
@@ -1,12 +1,14 @@
 #Day of week
 sq_monkey_data <- mutate(sq_monkey_data, Day_of_Week = wday(Date, label = TRUE))
 
-Fixing Misrecorded Observations (Damian -> Squirt)
+#Fixing Misrecorded Observations (Squirt is Damian Part 1)
+rm_indicies1 <- which(sq_monkey_data$Notes == "Recorded Squirt's behavior under Damian" &
+                          sq_monkey_data$Name == "Damian")
 
- for(i in which(sq_monkey_data$Notes == "Recorded Squirt's behavior under Damian")){
-  
-  if(sq_monkey_data$Name[i] == "Damian"){
-    
-   # sq_monkey_data$Name[i] <- "Squirt"
-  #}
-#}
+sq_monkey_data[rm_indicies1, which(names(sq_monkey_data) == "Name")] <- "Squirt"
+
+#Fixing Misrecorded Observations (Damian is Squirt Part 2)
+rm_indicies2 <- which(sq_monkey_data$Notes == "Used Damien's name for Squirt's behavior." &
+                          sq_monkey_data$Name == "Damian")
+
+sq_monkey_data[rm_indicies2, which(names(sq_monkey_data) == "Name")] <- "Squirt"
