89d528a02f8b40bb71ac30d38640f0ddca8e8391
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-03-11 17:10:13 -0500

    working

--- sq_monkey_columns_addition.R
+++ sq_monkey_columns_addition.R
@@ -1,11 +1,11 @@
 #Day of week
 sq_monkey_data <- mutate(sq_monkey_data, Day_of_Week = wday(Date, label = TRUE))
 
-#Fixing Misrecorded Observations (Damian -> Squirt)
+Fixing Misrecorded Observations (Damian -> Squirt)
 
- #for(i in which(sq_monkey_data$Notes == "Recorded Squirt's behavior under Damian")){
+ for(i in which(sq_monkey_data$Notes == "Recorded Squirt's behavior under Damian")){
   
-  #if(sq_monkey_data$Name[i] == "Damian"){
+  if(sq_monkey_data$Name[i] == "Damian"){
     
    # sq_monkey_data$Name[i] <- "Squirt"
   #}
