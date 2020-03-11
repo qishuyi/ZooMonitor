78b8836f4d34ad3459734da38f0eb4b2e12d0149
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-03-10 19:12:03 -0500

    Attemtped fixes, realized it is probably not possible

--- sq_monkey_columns_addition.R
+++ sq_monkey_columns_addition.R
@@ -1,2 +1,12 @@
 #Day of week
 sq_monkey_data <- mutate(sq_monkey_data, Day_of_Week = wday(Date, label = TRUE))
+
+#Fixing Misrecorded Observations (Damian -> Squirt)
+
+ #for(i in which(sq_monkey_data$Notes == "Recorded Squirt's behavior under Damian")){
+  
+  #if(sq_monkey_data$Name[i] == "Damian"){
+    
+   # sq_monkey_data$Name[i] <- "Squirt"
+  #}
+#}
