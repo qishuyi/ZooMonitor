d3cd51b16857d83e51172b1b5aedfc0a08e1da14
Riu Sakaguchi <sakaguch@grinnell.edu>
2020-03-25 14:00:38 -0700

    own changes

---
+++ Prototype_Program_Riu.R
@@ -0,0 +1,439 @@
+#Loading in Libraries
+library(readr)
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(lubridate)
+library(shiny)
+library(ggplot2)
+library(forcats)
+
+### Note: 
+### Currently, if no data file is uploaded, the APP will be using the generalized cleaning script by default,
+### which uses dogs' data (line 97). To use another dataset, either upload the data file or change the 
+### filepath in generalized_cleaning.R.
+
+# Helper functions to create plots
+
+############################### General Observations ###############################
+generalplot <- function(input, output, animal_data) {
+  ##Bar plot of observation distribution
+  output$general_plot <- renderPlot({
+    #Time of Day plot
+    if(input$select_general == "Time of Day"){
+      ggplot(data = animal_data, aes(x = Hour)) + 
+        geom_bar(aes(y = ..count../nrow(animal_data)*100), fill = "steelblue", width = .75) + 
+        scale_x_discrete(limits = 9:16) +
+        scale_y_continuous(limits = c(0,100)) +
+        labs(title = "Percentage of Observations (Time of Day)", x = "Time of Day", y = "Percentage (%)") + 
+        geom_hline(yintercept = (1/8)*100, color = "darkmagenta", alpha = .45, linetype = "longdash")
+    } 
+    # Day of Week Plot 
+    else if (input$select_general == "Day of Week"){
+      ggplot(data = animal_data, aes(x = Day_of_Week)) +
+        geom_bar(aes(y = ..count../nrow(animal_data)*100), fill = "steelblue2", width = .75) +
+        scale_x_discrete(limits=c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")) +
+        scale_y_continuous(limits = c(0,100)) +
+        labs(title = "Percentage of Observations (Day of Week)", x = "Day of Week", y = "Percentage (%)") +
+        geom_hline(yintercept = (1/7)*100, color = "darkmagenta", alpha = .45, linetype = "longdash") 
+    }
+    #Animal Plot
+    else {
+      a <- length(unique(animal_data$Name))
+      ggplot(data = animal_data, aes(x = Name)) +
+        geom_bar(aes(y = ..count../nrow(animal_data)*100), fill = "aquamarine3", width = .75) +
+        scale_y_continuous(limits = c(0,100)) +
+        labs(title = "Percentage of Observations (Animal's Name)", x = "Animal's Name", y = "Percentage (%)") +
+        geom_hline(yintercept = (1/a)*100, color = "darkmagenta", alpha = .45, linetype = "longdash") 
+    }
+  })
+}
+
+############################### Category ###############################
+category <- function(input, output, animal_data) {
+  # TODO: Create plot here
+}
+
+############################### Behavior ###############################
+behavior <- function(input, output, animal_data) {
+  # TODO: Create plot here
+}
+
+############################### Faceted Barplots ###############################
+facetedBarplots <- function(input, output, animal_data) {
+  # TODO: Create plot here
+}
+
+############################### Pie Charts ###############################
+piechart <- function(input, output, animal_data) {
+  output$event_pie_plot <- renderPlot({
+    #Creates a before dataset
+    before <- subset(animal_data, Date < input$date)
+    before <- before %>% group_by(Behavior)
+    summary_before <- as.data.frame(summarise(before, n()))
+    names(summary_before)[names(summary_before) == "n()"] <- "counts"
+    a_before <- sum(summary_before$counts)
+    summary_before <- summary_before %>%
+      mutate(Percent = round(counts/a_before*100, 1)) %>%
+      mutate(Period = "Before")
+    
+    #Creates an after dataset 
+    after <- subset(animal_data, Date > input$date)
+    after <- after %>% group_by(Behavior)
+    summary_after <- as.data.frame(summarise(after, n()))
+    names(summary_after)[names(summary_after) == "n()"] <- "counts"
+    a_after <- sum(summary_after$counts)
+    summary_after <- summary_after %>%
+      mutate(Percent = round(counts/a_after*100, 1)) %>%
+      mutate(Period = "After")
+    
+    #Combines two summaries
+    summary <- rbind(summary_before, summary_after)
+    summary$Period <- factor(summary$Period, levels = c("Before", "After"))
+    
+    ggplot(summary, aes(x="", y=Percent, fill=fct_reorder(Behavior, desc(Percent)))) + 
+      geom_bar(stat="identity", width=1) +
+      facet_grid(.~ Period) +
+      coord_polar("y", start=0) + 
+      labs(x = NULL, y = NULL, fill = NULL, title = "The Event and Behaviors",
+           subtitle = paste("Raw Counts: Before = ", a_before, ", After = ", a_after)) +
+      guides(fill = guide_legend(reverse = TRUE, override.aes = list(size = 1))) +
+      theme_classic() + theme(axis.line = element_blank(),
+                              axis.text = element_blank(),
+                              axis.ticks = element_blank(),
+                              plot.title = element_text(hjust = 0.5, face = "bold"),
+                              plot.subtitle = element_text(hjust = 0.5, face = "italic"),
+                              legend.position="bottom") +
+      scale_fill_manual(values = rainbow(length(unique(animal_data$Behavior)))[sample(1:length(unique(animal_data$Behavior)))])
+    
+    
+  })
+}
+
+
+############### UI and Server ###############
+ui <- navbarPage("ZooMonitor",
+                 ############################### Upload Data ###############################
+                 tabPanel("Upload Data",
+                          
+                          titlePanel("Upload a csv file"),
+                          
+                          sidebarLayout(
+                            # Add filters to take user inputs
+                            sidebarPanel(
+                              # Allow users to upload a csv file
+                              fileInput("file1", "Choose CSV File",
+                                        multiple = TRUE,
+                                        accept = c("text/csv",
+                                                   "text/comma-separated-values,text/plain",
+                                                   ".csv"))
+                            ),
+                            # Display the data file
+                            mainPanel(
+                              tableOutput("contents")
+                            ))),
+                 
+                 ############################### General Observations ###############################
+                 tabPanel("General Observations",
+                          
+                          titlePanel("Distribution of Observations"),
+                          
+                          sidebarLayout(
+                            # Add filters to take user inputs
+                            sidebarPanel(
+                              # Allow users to choose the x-axis
+                              radioButtons("select_general", "Choose an Input",
+                                           choices = list("Time of Day", "Day of Week", "Animal's Name")
+                              )
+                            ),
+                            mainPanel(
+                              # Show the plot of general obervations
+                              plotOutput("general_plot"),
+                              textOutput("selected_general"),
+                              h6("The dash line represents equally distributed observations.", align = "center")
+                              
+                            ))),
+                 
+                 ############################### Category ###############################
+                 tabPanel("Category",
+                          
+                          titlePanel("Barplot of Chosen Categories per Animal"),
+                          sidebarPanel(
+                            uiOutput("select_category")
+                            
+                          ),
+                          mainPanel(
+                            plotOutput(outputId = "category_output")
+                          )),
+                 
+                 ############################### Behavior ###############################
+                 tabPanel("Behavior",
+                          
+                          titlePanel("Placeholder for title 3"),
+                          sidebarPanel(
+                            # TODO: Add filters here to get user inputs
+                          ),
+                          mainPanel(
+                            # TODO: Create plots here
+                          )
+                 ),
+                 
+                 ############################### Faceted Barplot ###############################
+                 tabPanel("Faceted Barplot",
+                          
+                          titlePanel("Placeholder for title 4"),
+                          sidebarPanel(
+                            
+                            uiOutput("nameControls")
+                            
+                            # TODO: If necessary, add more filters here to get user inputs
+                          ),
+                          mainPanel(
+                            # TODO: Show plots here
+                          )),
+                 
+                 ############################### Pie Chart ###############################
+                 tabPanel("Pie Chart",
+                          
+                          titlePanel("Event's Impact on Behaviors"),
+                          
+                          sidebarLayout(
+                            # Add filters to take user inputs
+                            sidebarPanel(
+                              # Allow users to choose the x-axis
+                              uiOutput("dateControls"),
+                              helpText("The date can only be chosen from the dataset you uploaded.")
+                            ),
+                            mainPanel(
+                              #Removing the warning message that appears for a second 
+                              #This is a warning for not having either pie chart of before or after on the min/max date
+                              tags$style(type="text/css",
+                                         ".shiny-output-error { visibility: hidden; }",
+                                         ".shiny-output-error:before { visibility: hidden; }"),
+                              # Show the plot of general obervations
+                              plotOutput("event_pie_plot"),
+                              h6("The colors of slices will change every time you change the date.", align = "center")
+                            ))))
+
+# Define server logic
+server <- function(input, output) {
+  
+  source("generalized_cleaning.R")
+  
+  ############################### Upload Data ###############################
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially, the req function ensures the value of input$file1 is available.
+    # If input$file1 is unavailable, req will throw an exception.
+    req(input$file1)
+    
+    #Loading in Data
+    animal_data <- read_csv(input$file1$datapath, 
+                            col_types = cols(.default = col_character(),
+                                             SessionID = col_double(), 
+                                             `Session Start Time` = col_datetime(format = ""),
+                                             `Session End Time` = col_datetime(format = ""),
+                                             DateTime = col_datetime(format = ""),
+                                             Date = col_date(format = ""),
+                                             Time = col_time(format = ""),
+                                             Year = col_double(),
+                                             Month = col_double(),
+                                             Hour = col_double(),
+                                             Duration = col_double(),
+                                             `Frame Number` = col_double()))
+    
+    #Removing spaces and adding underscore
+    names(animal_data) <- gsub(" ", "_", names(animal_data))
+    
+    
+    #Renaming Columns
+    animal_data <- animal_data %>% rename(Name = Focal_Name)
+    
+    
+    #Creating a Duplicate Channel Type Column if All_Occurrence_Value exists as a column
+    #If All_Occurrence Value exists as a column, also adds "Occurrence_Duplicate" into the IC_Name_Vector
+    #If All_Occurrence Value exists as a column, also adds "All_Occurrence_Value" into the IC_Value_Vector
+    
+    #Creating vectors used to unite to create a "Category" and "Activity" Column
+    IC_Name_Vector <- character()
+    IC_Value_Vector <- character()
+    
+    if("All_Occurrence_Value" %in% names(animal_data)){
+      animal_data <- cbind(animal_data, Occurrence_Duplicate = animal_data$Channel_Type)
+      animal_data$Occurrence_Duplicate[animal_data$Occurrence_Duplicate == "Interval"] <- NA
+      IC_Name_Vector <- append(IC_Name_Vector, "Occurrence_Duplicate")
+      IC_Value_Vector <- append(IC_Value_Vector, "All_Occurrence_Value")
+      
+    }
+    
+    #Uniting all IC Names + Duplicated All Occurrence columns to a single column named Category    
+    
+    for(i in names(animal_data)){
+      
+      if(str_detect(i, "^Interval_Channel.+Name$")){ 
+        
+        IC_Name_Vector <- append(IC_Name_Vector, i)
+      }
+    }
+    
+    animal_data <- animal_data %>% unite("Category", IC_Name_Vector , remove = T)                                                                 
+    
+    
+    #Uniting all IC Value + Occurrence Value columns to a single column named Activity   
+    
+    for(j in names(animal_data)){
+      
+      if(str_detect(j, "^Interval_Channel.+Value$")){ 
+        
+        IC_Value_Vector <- append(IC_Value_Vector, j)
+      }
+    }
+    
+    animal_data <- animal_data %>% unite("Behavior", IC_Value_Vector, remove = T)
+    
+    #Social Modifier Editing
+    
+    #Do nothing if there is no social modifier column
+    #If there is only one social modifier column, rename the column to Social_Modifier
+    #If there are more than one social modifier column, unite it into a single column 
+    
+    Social_Modifier_Vector <- character()
+    
+    for(k in names(animal_data)){
+      
+      if(str_detect(k, "Social_Modifier$")){
+        
+        Social_Modifier_Vector <- append(Social_Modifier_Vector, k)
+      }
+      
+    }
+    
+    
+    if(length(Social_Modifier_Vector) == 1){
+      animal_data <- animal_data %>% rename(Social_Modifier = Social_Modifier_Vector[1])
+      
+    } else if(length(Social_Modifier_Vector) > 1){
+      animal_data <- animal_data %>% unite("Social_Modifier", Social_Modifier_Vector, remove = TRUE)
+    }
+    
+    
+    #Filtering out rows without behavioral observations
+    animal_data <- animal_data %>% filter(!str_detect(animal_data$Behavior, "^NA(_NA)*$")) 
+    
+    
+    #Fixing NA labels for Category Column
+    animal_data <- animal_data %>% mutate(Category = gsub("NA_", "", Category)) %>%  
+      mutate(Category = gsub("_NA", "", Category)) 
+    
+    #Fixing NA labels for Behavior Column
+    animal_data <- animal_data %>% mutate(Behavior = gsub("NA_", "", Behavior)) %>%  
+      mutate(Behavior = gsub("_NA", "", Behavior)) 
+    
+    #Fixing NA labels for Social Modifier Column
+    animal_data <- animal_data %>% mutate(Social_Modifier = gsub("NA_", "", Social_Modifier)) %>%
+      mutate(Social_Modifier = gsub("_NA", "", Social_Modifier)) 
+    
+    if(length(Social_Modifier_Vector) > 1){
+      animal_data$Social_Modifier[animal_data$Social_Modifier == "NA"] <- NA
+    }
+    
+    #Splitting rows with multiple behavioral observations                                
+    animal_data <- separate_rows(animal_data, Category, Behavior, sep  = "_")
+    
+    
+    #Removing Unnecessary Columns
+    animal_data <- animal_data %>% select(-Configuration_Name, -Observer,-DeviceID,
+                                          -DateTime, -Grid_Size, -Image_Size,
+                                          -Project_Animals, -Duration, -Channel_Type)
+    
+    ####################### Addition
+    #Adding Day of Week
+    animal_data <- mutate(animal_data, Day_of_Week = wday(Date, label = TRUE))
+    
+    
+    ############################### General Observations ###############################
+    generalplot(input, output, animal_data)
+    
+    ############################### Category ###############################
+    # TODO: Create the reactive filter options here
+    
+    output$select_category <- renderUI({
+      category <- unique(animal_data$Category)
+      checkboxGroupInput(inputId = "category_input", 
+                         label= "Select Categories",
+                         choices = category)
+      
+    })
+   
+    
+    #Creates Barplots based on Chosen Behavior
+    category(input, output, animal_data)
+    
+    ############################### Behavior ###############################
+    # TODO: Create the reactive filter options here
+    behavior(input, output, animal_data)
+    
+    ############################### Faceted Barplots ###############################
+    #Add filter options for animal names based on the input data file
+    output$nameControls <- renderUI({
+      names <- unique(animal_data$Name)
+      checkboxGroupInput('names', "Choose Animal", names)
+    })
+    #Create the faceted barplots
+    facetedBarplots(input, output, animal_data)
+    
+    ############################### Pie Charts ###############################
+    output$dateControls <- renderUI({
+      date <- animal_data$Date
+      dateInput("date", "Date of the event:",
+                value = min(animal_data$Date),
+                min = min(animal_data$Date),
+                max = max(animal_data$Date))
+    })
+    piechart(input, output, animal_data)
+    
+    return(animal_data)
+    
+  })
+  
+  ## Note: Everything below that's within the server function might be deleted in the final version,
+  ##       depending on whether we want to provide a default data file for the users.
+  
+  ############################### General Observations ###############################
+  generalplot(input, output, animal_data)
+  
+  ############################### Category ###############################
+  # TODO: Create the reactive filter options here
+  output$select_category <- renderUI({
+    category <- unique(animal_data$Category)
+    checkboxGroupInput(inputId = "category_input", 
+                       label= "Select Categories",
+                       choices = category)
+    
+  })
+  
+  ############################### Behavior ###############################
+  # TODO: Create the reactive filter options here
+  behavior(input, output, animal_data)
+  
+  ############################### Faceted Barplots ###############################
+  # This function is here because it will be easier to not have to upload data files every time we try to
+  # run the APP during the prototyping process. It probably will be removed in the final APP.
+  output$nameControls <- renderUI({
+    names <- unique(animal_data$Name)
+    checkboxGroupInput('names', "Choose Animal", names)
+  })
+  
+  ############################### Pie Charts ###############################
+  output$dateControls <- renderUI({
+    date <- animal_data$Date
+    dateInput("date", "Date of the event:",
+              value = min(animal_data$Date),
+              min = min(animal_data$Date),
+              max = max(animal_data$Date))
+  })
+  piechart(input, output, animal_data)
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
