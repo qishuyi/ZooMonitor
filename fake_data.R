e1a9cd9d07aa5d65c2591475dd632c8493996cfb
Shuyi Qi <qishuyi0525@gmail.com>
2020-04-12 18:20:41 -0500

    Fake data script done (except write to csv)

--- fake_data.R
+++ fake_data.R
@@ -1,13 +1,11 @@
+library(manip)
 
 #Function that generates fake data
 create_fake_data <- function(data){
+  data <- read.csv("https://raw.githubusercontent.com/qishuyi/ZooMonitor/master/Data/report_study_1582236321.csv")
   
   #length of data
   data_length <- nrow(data)
-  
-  #This is just to make the code easier to write, we will revert it back after we are done
-  data <- data %>% rename(Name = `Focal Name`)
-  
   
   #Vector of fake names
   fake_names <- c("Lincoln", "Abigail", "Zion", "Wilmer", "Dolly", "Strike", "Kale", "Amor", 
@@ -15,8 +13,8 @@ create_fake_data <- function(data){
                   "Rose", "Azure", "Fluff", "Leroy", "Remy", "Nova", "Bucky", "Tofu", "Elton",
                   "Mini", "Ollie", "Eddie", "Paris", "Argentina") 
    
-  #Actual names in the data
-  actual_names <- unique(data$Name)               
+  #Actual animal names in the data
+  actual_names <- unique(data$Focal.Name)               
   
   #Number of animals in the data
   name_length <- length(actual_names)
@@ -24,29 +22,44 @@ create_fake_data <- function(data){
   #Vector of fake names to replace the real names
   new_names <- sample(fake_names, name_length)
   
-  #Count to access new_names in the loop
-  count <- 0
+  #Create a vector of indices for the columns where we want to manipulate names
+  social_modifier_column_indicies <- which(str_detect(colnames(data), "Social.Modifier"))
+  social_modifier_column_indicies <- c(which(colnames(data) == "Focal.Name"), social_modifier_column_indicies)
   
-  #Replacing names
-  for(i in actual_names){
-    count <- count + 1
-    
-    for(j in 1:data_length){
-      
-      if(i == data$Name[j]){
-        
-        data$Name[j] <- new_names[count]
-      }
-      
+  #Create fake names for each animal name in the dataset
+  for (i in 1:length(actual_names)) {
+    for (j in social_modifier_column_indicies) {
+      data[,j] <- as.character(data[,j])
+      indices <- which(data[,j] == actual_names[i])
+      data[,j] <- replace(data[,j], indices, rep.int(c(new_names[i]), length(indices))) 
+      data[,j] <- as.factor(data[,j])
     }
-    
+    #Change project animals too
+    data$Project.Animals <- gsub(actual_names[i], new_names[i], data$Project.Animals)
   }
   
+  #Actual Observer names in the data
+  observer_names <- unique(data$Observer)   
   
-  #Renaming it back to Focal Name
-  data <- data %>% rename(`Focal Name` = Name)
+  #Number of observers in the data
+  observer_name_length <- length(observer_names)
+  
+  #Vector of fake names to replace the real names
+  new_observer_names <- sample(fake_names, observer_name_length)
+  
+  #Create fake names for each observer name in the dataset
+  for (i in 1:length(observer_names)) {
+    data$Observer <- as.character(data$Observer)
+    indices <- which(data$Observer == observer_names[i])
+    data$Observer <- replace(data$Observer, indices, rep.int(c(new_observer_names[i]), length(indices))) 
+    data$Observer <- as.factor(data$Observer)
+  }
+  
+  #Eliminate animal species
+  data$Configuration.Name <- as.character(data$Configuration.Name)
+  data$Configuration.Name <- replace(data$Configuration.Name, 1:length(data$Configuration.Name), rep.int(c("Animal"), length(data$Configuration.Name)))
+  data$Configuration.Name <- as.factor(data$Configuration.Name)
  
-
   #Subsetting the 80% of the data randomly
   selected_rows <- sort(sample(1:data_length, ceiling(.8 * data_length), replace = F))
   
@@ -56,4 +69,4 @@ create_fake_data <- function(data){
  
 }
 
-
+test <- create_fake_data(read.csv("https://raw.githubusercontent.com/qishuyi/ZooMonitor/master/Data/report_study_1582236321.csv"))
